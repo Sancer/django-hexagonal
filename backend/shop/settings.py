a5e98365b297a0636b6170240fc3882a25ac4243
Uriel <urielreina@gmail.com>
2023-04-01 19:33:37 +0200

    feat(catalog): create a base context

--- backend/shop/settings.py
+++ backend/shop/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     "authentication.infrastructure.apps.AuthenticationConfig",
+    "catalog.infrastructure.apps.CatalogConfig",
 ]
 
 MIDDLEWARE = [
